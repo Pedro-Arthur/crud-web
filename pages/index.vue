ff97b8fa50d3b45938bde7b4b4b3838467e30877
Pedro Arthur <pedro13arthur@gmail.com>
2022-01-26 09:32:06 -0300

    first

---
+++ pages/index.vue
@@ -0,0 +1,227 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    sort-by="id"
+    class="elevation-1"
+    :footer-props="{
+      'items-per-page-text': 'Usuários por página',
+    }"
+  >
+    <template #top>
+      <v-toolbar flat>
+        <v-toolbar-title>Usuários</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template #activator="{ on, attrs }">
+            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+              Adicionar
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.id"
+                      label="ID"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Nome"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.job"
+                      label="Trabalho"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.age"
+                      label="Idade"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.phone"
+                      label="Telefone"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">
+                Cancelar
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Salvar </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >Tem certeza de que deseja excluir este usuário?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete"
+                >Cancelar</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <!-- eslint-disable-next-line vue/valid-v-slot -->
+    <template #item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+    <template #no-data>
+      <v-btn color="primary" @click="initialize"> Resetar </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'IndexItem',
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: 'ID',
+        align: 'start',
+        value: 'id',
+      },
+      { text: 'Nome', value: 'name' },
+      { text: 'Trabalho', value: 'job' },
+      { text: 'Idade', value: 'age' },
+      { text: 'Telefone', value: 'phone' },
+    ],
+    items: [],
+    editedIndex: -1,
+    editedItem: {
+      id: 0,
+      name: '',
+      job: '',
+      age: 0,
+      phone: '',
+    },
+    defaultItem: {
+      id: 0,
+      name: '',
+      job: '',
+      age: 0,
+      phone: '',
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'Novo usuário' : 'Editar usuário'
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created() {
+    this.initialize()
+  },
+
+  methods: {
+    initialize() {
+      this.items = [
+        {
+          id: 1,
+          name: 'Pedro',
+          job: 'Programador',
+          age: 19,
+          phone: '3498921188',
+        },
+        {
+          id: 2,
+          name: 'João',
+          job: 'UI/UX',
+          age: 22,
+          phone: '34994878787',
+        },
+        {
+          id: 3,
+          name: 'Lucas',
+          job: 'Suporte',
+          age: 32,
+          phone: '3499912121',
+        },
+      ]
+    },
+
+    editItem(item) {
+      this.editedIndex = this.items.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.items.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm() {
+      this.items.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.items[this.editedIndex], this.editedItem)
+      } else {
+        this.items.push(this.editedItem)
+      }
+      this.close()
+    },
+  },
+}
+</script>
